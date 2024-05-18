faadeefcff35088699b6386f0fe73a4433ffb413
Miroslav Tsarev <mtzarev@gmail.com>
2024-05-18 21:37:42 +0300

    Update test11.js

--- test11.js
+++ test11.js
@@ -2,7 +2,6 @@ function test11(input) {
     let length = input.length / 2;
     let first = (input.substring(0, length).split(``)).reverse();
     let second = input.substring(length).split(``).reverse();
-
     console.log(first.join(``));
     console.log(second.join(``));
 }
